efdd8c6a99f7eb3348242339b10a00af89050865
OlivierIrac <olivier.irac@gmail.com>
2017-09-07 18:09:13 +0200

    logpuzzle exercice

--- Google Python Days/logpuzzle/logpuzzle.py
+++ Google Python Days/logpuzzle/logpuzzle.py
@@ -20,42 +20,74 @@ Here's what a puzzle url looks like:
 
 
 def read_urls(filename):
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
-  
+    """Returns a list of the puzzle urls from the given log file,
+    extracting the hostname from the filename itself.
+    Screens out duplicate urls and returns the urls sorted into
+    increasing order."""
+    f = open(filename, "rU")
+    text = f.read()
+    f.close()
+    URLsList = []
+    # year = re.search(r'<h3 align="center">Popularity in (\d\d\d\d)</h3>', text)
+    # namesList.append(year.group(1))
+    finds = re.findall(r'GET (\S*puzzle\S*.jpg) ', text)
+    for find in finds:
+        imageName = find.split('/')[-1]
+        URLsList.append('https://developers.google.com/edu/python/images/puzzle/' + imageName)
+    # remove duplicates, keep only highest rank
+    URLsList.sort()
+    lastURL = ''
+    for URL in URLsList:
+        if URL == lastURL:
+            URLsList.remove(URL)
+        lastURL = URL
+    return URLsList
 
-def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
-  # +++your code here+++
-  
+
+def download_images(img_urls, todir):
+    """Given the urls already in the correct order, downloads
+    each image into the given directory.
+    Gives the images local filenames img0, img1, and so on.
+    Creates an index.html in the directory
+    with an img tag to show each local image file.
+    Creates the directory if necessary.
+    """
+    imgIndex = 0
+    if len(todir):
+        if (not os.path.exists(todir)):
+            os.makedirs(todir)
+    for url in img_urls:
+        imgFilename = os.path.join(todir, 'img' + str(imgIndex) + '.jpg')
+        print 'Downloading ' + imgFilename + ' from: ' + url
+        urllib.urlretrieve(url, imgFilename)
+        imgIndex += 1
+    f = open(os.path.join(todir, 'index.html'), "w")
+    f.write("<html><body>")
+    for i in range(imgIndex):
+        f.write("<img src=\"img" + str(i) + ".jpg\">")
+    f.write("</body></html>")
+    f.close()
+
 
 def main():
-  args = sys.argv[1:]
+    args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--todir dir] logfile '
-    sys.exit(1)
+    if not args:
+        print 'usage: [--todir dir] logfile '
+        sys.exit(1)
 
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  img_urls = read_urls(args[0])
+    img_urls = read_urls(args[0])
 
-  if todir:
-    download_images(img_urls, todir)
-  else:
-    print '\n'.join(img_urls)
+    if todir:
+        download_images(img_urls, todir)
+    else:
+        print '\n'.join(img_urls)
+
 
 if __name__ == '__main__':
-  main()
+    main()
